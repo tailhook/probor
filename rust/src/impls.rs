b342bf9e9fd3ab56937822d7a0d8918f70278f77
Paul Colomiets <paul@colomiets.name>
2015-08-12 01:20:47 +0300

    Add implementation of Decodable for different types of int

--- rust/src/impls.rs
+++ rust/src/impls.rs
@@ -69,3 +69,127 @@ impl Encodable for u64 {
         e.u64(*self)
     }
 }
+
+impl Decodable for u32 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.u32())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for u32 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.u32(*self)
+    }
+}
+impl Decodable for u16 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.u16())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for u16 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.u16(*self)
+    }
+}
+
+impl Decodable for u8 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.u8())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for u8 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.u8(*self)
+    }
+}
+
+impl Decodable for i64 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.i64())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for i64 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.i64(*self)
+    }
+}
+
+impl Decodable for i32 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.i32())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for i32 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.i32(*self)
+    }
+}
+impl Decodable for i16 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.i16())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for i16 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.i16(*self)
+    }
+}
+
+impl Decodable for i8 {
+    fn decode_opt<R: Input>(e: &mut Decoder<R>)
+        -> Result<Option<Self>, DecodeError>
+    {
+        // TODO(tailhook) implement text iterator
+        let res = try!(opt(e.i8())
+            .map_err(|e| DecodeError::ExpectationFailed("string expected", e)));
+        Ok(res)
+    }
+}
+
+impl Encodable for i8 {
+    fn encode<W: Output>(&self, e: &mut Encoder<W>) -> Result<(), EncodeError>
+    {
+        e.i8(*self)
+    }
+}
