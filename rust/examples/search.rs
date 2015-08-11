e3638614ffc5a6dcefe56ba36e23bbbb7db1a573
Paul Colomiets <paul@colomiets.name>
2015-08-11 17:52:17 +0300

    Add example

---
+++ rust/examples/search.rs
@@ -0,0 +1,42 @@
+#[macro_use] extern crate probor;
+
+use probor::{Encoder, Encodable};
+use probor::{Decoder, Config, decode};
+use std::io::Cursor;
+
+probor_struct!(
+#[derive(PartialEq, Eq, Debug)]
+Page {
+    url: String => (#0),
+    title: String => (#1),
+    snippet: Option<String> => (#2 optional),
+});
+
+probor_struct!(
+#[derive(PartialEq, Eq, Debug)]
+SearchResults {
+    total_results: u64 => (#0),
+    results: Vec<Page> => (#1),
+});
+
+
+fn main() {
+    let buf = Vec::new();
+    let mut enc = Encoder::new(buf);
+    SearchResults {
+        total_results: 112,
+        results: vec![Page {
+            url: "http://url1.example.com".to_string(),
+            title: "One example".to_string(),
+            snippet: None,
+        }, Page {
+            url: "http://url2.example.com".to_string(),
+            title: "Two example".to_string(),
+            snippet: Some("Example Two".to_string()),
+        }],
+    }.encode(&mut enc).unwrap();
+    let sr: SearchResults = decode(
+        &mut Decoder::new(Config::default(), Cursor::new(enc.into_writer())))
+        .unwrap();
+    println!("Results {:?}", sr);
+}
